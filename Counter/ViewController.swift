17733db510b2f3ce125bbe9aee023c7db101cc75
Jakosh999 <developer.superpower@gmail.com>
2019-04-18 10:59:57 +0600

    The counter with pause and reset options

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var countedNumber = Int()
+    
+    
 
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        scoreLabel.isHidden = true
     }
 
-
+    
+    @IBAction func resetPressed(_ sender: AnyObject) {
+        updateScore()
+        countedNumber = 0
+        countLabel.text = String(0)
+    }
+    
+    @IBAction func pausePressed(_ sender: Any) {
+        updateScore()
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        countedNumber += 1
+        countLabel.text = String(countedNumber)
+    }
+    
+    func updateScore() {
+        scoreLabel.isHidden = false
+        scoreLabel.text = "Total score:  \(countedNumber)"
+    }
+    
 }
 
